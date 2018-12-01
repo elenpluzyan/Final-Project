cdddfe6337b6da878e565f5cf7a0e27de8c4ddbd
elenpluzyan <elen_pluzyan@edu.aua.am>
2018-12-01 14:44:52 +0400

    Adding last changes

--- model.js
+++ model.js
@@ -1,13 +1,13 @@
 function populateModel(s1,s2){
-	var s1 = document.getElementById(s1);
-	var s2 = document.getElementById(s2);
+	 s1 = document.getElementById(s1);
+	 s2 = document.getElementById(s2);
 	s2.innerHTML = "";
 	if(s1.value == "Body"){
-		var optionArray = ["|","3dmodel|3D Human","robot|Robot","abstract|Abstract Human"];
+		const optionArray = ["|","3dmodel|3D Human","robot|Robot","abstract|Abstract Human"];
 	} 
-	for(var option in optionArray){
-		var pair = optionArray[option].split("|");
-		var newOption = document.createElement("option");
+	for(const option in optionArray){
+		const pair = optionArray[option].split("|");
+		const newOption = document.createElement("option");
 		newOption.value = pair[0];
 		newOption.innerHTML = pair[1];
 		s2.options.add(newOption);
