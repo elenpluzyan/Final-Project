cf923ebd163fb7e54ebab5fb2457042626cea8d4
elenpluzyan <elen_pluzyan@edu.aua.am>
2018-12-01 15:26:57 +0400

    testing

--- hairbutton.js
+++ hairbutton.js
@@ -2,7 +2,7 @@ function populateHair(s0,s1,s2,s3){
 	var s1 = document.getElementById(s1);
 	var s2 = document.getElementById(s2);
 
-	debugger;
+
 	s2.innerHTML = "";
 	if(s1.value =="Hair"){
 		var optionArray = ["|","short|Short","medium|Medium","long|Long"]
